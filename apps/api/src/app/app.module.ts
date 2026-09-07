d50e4e81cea124eb7de0607bbbaf81fba88791c5
tavysmikael <tavysmikael@gmail.com>
2026-09-07 13:58:10 -0300

    fix: remove specs sem jest configurado no modulo verses

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { PrismaModule } from './prisma/prisma.module';
+import { VersesModule } from './verses/verses.module';
+
 @Module({
-  imports: [],
+  imports: [PrismaModule, VersesModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
