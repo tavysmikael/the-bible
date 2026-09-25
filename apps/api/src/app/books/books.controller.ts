127a0474540a4c530d9a1ec5a456a425e1532342
tavysmikael <tavysmikael@gmail.com>
2026-09-24 22:40:01 -0300

    feat: adiciona backend para busca de livros, insensitive,etc

--- apps/api/src/app/books/books.controller.ts
+++ apps/api/src/app/books/books.controller.ts
@@ -1,13 +1,20 @@
-// books.controller.ts
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+
 import { BooksService } from './books.service';
 
 @Controller('books')
 export class BooksController {
+
   constructor(private readonly booksService: BooksService) { }
 
   @Get()
   findAll() {
     return this.booksService.findAll();
   }
+
+  @Get('search')
+  search(@Query('q') q: string) {
+    return this.booksService.findBySlugOrName(q);
+  }
+
 }
--- apps/api/src/app/books/books.service.ts
+++ apps/api/src/app/books/books.service.ts
@@ -8,7 +8,25 @@ export class BooksService {
     findAll() {
         return this.prisma.book.findMany({
             orderBy: { order: 'asc' },
-            select: { id: true, slug: true, name: true, testament: true },
+            select: {
+                id: true,
+                slug: true,
+                name: true,
+                testament: true,
+            },
+        });
+    }
+
+    async findBySlugOrName(query: string) {
+        const normalized = query.toLowerCase();
+
+        return this.prisma.book.findFirst({
+            where: {
+                OR: [
+                    { slug: { contains: normalized } },
+                    { name: { contains: normalized, mode: 'insensitive' } },
+                ],
+            },
         });
     }
 }
